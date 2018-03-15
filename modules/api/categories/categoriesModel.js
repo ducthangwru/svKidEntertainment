493815622d4f85b4fb485a4126cf9f9b7b18c48f
DucThangWRU <ducthangwru97@gmail.com>
2018-03-15 08:05:36 +0700

    update

--- modules/api/categories/categoriesModel.js
+++ modules/api/categories/categoriesModel.js
@@ -14,7 +14,55 @@ const findByIdMenu = async (idmenu) => {
     }
 }
 
+const createCategory = async (category) => {
+    try
+    {
+        return await categoriesModel.create(category);
+    }
+    catch(err)
+    {
+        console.log(err);
+        return null;
+    }
+}
+
+const updateCategory = async (category) => {
+    try
+    {
+        try
+        {
+            var id = category._id;
+            var queryUpdate = {
+                name : category.name,
+                image : category.image,
+                description : category.description
+            }
+        
+            return await categoriesModel.findOneAndUpdate(id, queryUpdate).exec();
+        }
+        catch(err)
+        {
+            return null;
+        }
+    }
+    catch(err)
+    {
+        return null;
+    }
+}
+
+const removeCategory = async (id) => {
+    try
+    {
+        return await categoriesModel.remove({_id : id}).exec();
+    }
+    catch(err)
+    {
+        return null;
+    }
+}
+
 
 module.exports = {
-    findByIdMenu
+    findByIdMenu, createCategory, updateCategory, removeCategory
 }
--- modules/api/categoryDetails/categoryDetailsModel.js
+++ modules/api/categoryDetails/categoryDetailsModel.js
@@ -14,7 +14,56 @@ const findByIdCategory = async (idCategory) => {
     }
 }
 
+const createCategoryDetail = async(categoryDetail) => {
+    try
+    {
+        return await categoryDetailsModel.create(categoryDetail);
+    }
+    catch(err)
+    {
+        console.log(err);
+        return null;
+    }
+}
+
+
+const updateCategoryDetail = async (categoryDetail) => {
+    try
+    {
+        try
+        {
+            var id = categoryDetail._id;
+            var queryUpdate = {
+                category : categoryDetail.category,
+                link : categoryDetail.link,
+                description : categoryDetail.description
+            }
+        
+            return await categoryDetailsModel.findOneAndUpdate(id, queryUpdate).exec();
+        }
+        catch(err)
+        {
+            return null;
+        }
+    }
+    catch(err)
+    {
+        return null;
+    }
+}
+
+const removeCategoryDetail = async (id) => {
+    try
+    {
+        return await categoryDetailsModel.remove({_id : id}).exec();
+    }
+    catch(err)
+    {
+        return null;
+    }
+}
+
 
 module.exports = {
-    findByIdCategory
+    findByIdCategory, createCategoryDetail, updateCategoryDetail, removeCategoryDetail
 }
